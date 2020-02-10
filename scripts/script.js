bdca32e13b189e98695da6df20789c90b0e1e7ab
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-02-10 21:00:20 +0000

    Commented out drop list for future implimentation

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,23 @@
 
-fetch("https://opentdb.com/api_category.php")
-    .then(function (response) {
-        return response.json(); //Returns API Data as JSON
-    })
-    .then(function (category) { 
-        let categoryList = category.trivia_categories; //Access category list
-        categoryList.forEach(function (category) {
+//fetch("https://opentdb.com/api_category.php")
+  //  .then(function (response) {
+    //    return response.json(); //Returns API Data as JSON
+    //})
+    //.then(function (category) { 
+      //  let categoryList = category.trivia_categories; //Access category list
+        //categoryList.forEach(function (category) {
 
-            let categoryOption = document.createElement("option"); //Creates Option Item In DOM
-            let categoryName = document.createElement("p"); //Creates Nested <p> tags 
-            let name = document.createTextNode(category.name); //Defines name of the item
+          //  let categoryOption = document.createElement("option"); //Creates Option Item In DOM
+            //let categoryName = document.createElement("p"); //Creates Nested <p> tags 
+            //let name = document.createTextNode(category.name); //Defines name of the item
             
-            categoryName.appendChild(name);
-            categoryOption.appendChild(categoryName);
-            categoryOption.id = category.id; //adds id ref to <option> tag
-            categoryOption.classList.add("category"); //adds class to <option> tag
-            document.getElementById("categoryList").appendChild(categoryOption); //items to be added to categoryList class item in DOM
-        });
-    });
+           // categoryName.appendChild(name);
+           // categoryOption.appendChild(categoryName);
+            //categoryOption.id = category.id; //adds id ref to <option> tag
+            //categoryOption.classList.add("category"); //adds class to <option> tag
+            //document.getElementById("categoryList").appendChild(categoryOption); //items to be added to categoryList class item in DOM
+       // });
+    //});
 
 
 
