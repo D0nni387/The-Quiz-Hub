364b7b1c3f8bd6413df871ecfc3bd22e9cf7e783
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-02-10 20:46:24 +0000

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,29 @@
+<<<<<<< HEAD
 const length = 10
+=======
+let baseURL = "https://opentdb.com/api.php?amount="
+
+>>>>>>> parent of 30063b0... Updated base url and fetch template
 
 fetch("https://opentdb.com/api_category.php")
     .then(function (response) {
         return response.json(); //Returns API Data as JSON
     })
-    .then(function (category) {
+    .then(function (category) { 
         let categoryList = category.trivia_categories; //Access category list
         categoryList.forEach(function (category) {
 
             let categoryOption = document.createElement("option"); //Creates Option Item In DOM
             let categoryName = document.createElement("p"); //Creates Nested <p> tags 
             let name = document.createTextNode(category.name); //Defines name of the item
-
+            
             categoryName.appendChild(name);
             categoryOption.appendChild(categoryName);
             categoryOption.id = category.id; //adds id ref to <option> tag
             categoryOption.classList.add("category"); //adds class to <option> tag
             document.getElementById("categoryList").appendChild(categoryOption); //items to be added to categoryList class item in DOM
         });
+<<<<<<< HEAD
     });
 
 
@@ -34,3 +40,6 @@ function getSelection(id, length) {
             newQuiz(data.results);
         });
 }
+=======
+    })
+>>>>>>> parent of 30063b0... Updated base url and fetch template
