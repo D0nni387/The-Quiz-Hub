c5d479220a0ad5b77b93769801ec40f4556f0b45
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-02-10 20:56:08 +0000

    Revert "Updated base url and fetch template"
    This reverts commit 30063b09623f4932bb4c2f90fc0efbea462d0cad.

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-const length = 10
-=======
-let baseURL = "https://opentdb.com/api.php?amount="
-
->>>>>>> parent of 30063b0... Updated base url and fetch template
 
 fetch("https://opentdb.com/api_category.php")
     .then(function (response) {
@@ -23,23 +17,7 @@ fetch("https://opentdb.com/api_category.php")
             categoryOption.classList.add("category"); //adds class to <option> tag
             document.getElementById("categoryList").appendChild(categoryOption); //items to be added to categoryList class item in DOM
         });
-<<<<<<< HEAD
     });
 
 
 
-function getSelection(id, length) {
-    let baseURL = "https://opentdb.com/api.php?amount=";
-    let fetchQuiz = baseURL + length + "&category=" + id + "&type=multiple";
-
-    fetch(fetchQuiz)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            newQuiz(data.results);
-        });
-}
-=======
-    })
->>>>>>> parent of 30063b0... Updated base url and fetch template
