bb3866ddd78b27eb3cef93df3161579bfe86800b
D0nni387 <Richard.wells1987@hotmail.co.uk>
2020-02-12 20:55:54 +0000

    Readded category select script

--- scripts/script.js
+++ scripts/script.js
@@ -1,59 +1,68 @@
-//fetch("https://opentdb.com/api_category.php")
-//  .then(function (response) {
-//    return response.json(); //Returns API Data as JSON
-//})
-//.then(function (category) { 
-//  let categoryList = category.trivia_categories; //Access category list
-//categoryList.forEach(function (category) {
+getCategories = () => {
 
-//  let categoryOption = document.createElement("option"); //Creates Option Item In DOM
-//let categoryName = document.createElement("p"); //Creates Nested <p> tags 
-//let name = document.createTextNode(category.name); //Defines name of the item
+    fetch("https://opentdb.com/api_category.php")
 
-// categoryName.appendChild(name);
-// categoryOption.appendChild(categoryName);
-//categoryOption.id = category.id; //adds id ref to <option> tag
-//categoryOption.classList.add("category"); //adds class to <option> tag
-//document.getElementById("categoryList").appendChild(categoryOption); //items to be added to categoryList class item in DOM
-// });
-//});
+        .then(function (response) {
+            return response.json(); //Returns API Data as JSON
+        })
+        .then(function (category) {
+            let categoryList = category.trivia_categories; //Access category list
+            categoryList.forEach(function (category) {
 
+                let categoryOption = document.createElement("option"); //Creates Option Item In DOM
+                let categoryName = document.createElement("p"); //Creates Nested <p> tags 
+                let name = document.createTextNode(category.name); //Defines name of the item
 
+                categoryName.appendChild(name);
+                categoryOption.appendChild(categoryName);
+                categoryOption.id = category.id; //adds id ref to <option> tag
+                categoryOption.classList.add("category"); //adds class to <option> tag
+                document.getElementById("categoryList").appendChild(categoryOption); //items to be added to categoryList class item in DOM
+            });
+        });
+}
+
+getCategories()
+
+//Main Quiz Functions
 
 let currentQuestion = {}
 let answers = Array.from(document.getElementsByClassName("answer-format"))
 let question = document.getElementById("question");
 let questions = []; //sets blank array for API to populate
 
-fetch("https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple")
-    .then(data => {
-        return data.json() //converts received data to JSON
-    })
-    .then(loadedQuestions => {
-        questions = loadedQuestions.results.map(loadedQuestion => {
-            const formatQuestion = {
-                question: loadedQuestion.question
-            }
+getQuiz = () => {
 
-            formatQuestion.answer = Math.floor(Math.random() * 3) + 1
-
-            const answerChoices = [...loadedQuestion.incorrect_answers]
-            answerChoices.splice(
-                formatQuestion.answer - 1, 0, loadedQuestion.correct_answer
-            ) //adds correct answer into the array
-            answerChoices.forEach((answer, index) => {
-                formatQuestion["choice" + (index + 1)] = answer
-            })
-
-            return formatQuestion
+    fetch("https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple")
+        .then(data => {
+            return data.json() //converts received data to JSON
         })
-        startGame()
-    })
+        .then(loadedQuestions => {
+            questions = loadedQuestions.results.map(loadedQuestion => {
+                const formatQuestion = {
+                    question: loadedQuestion.question
+                }
 
-startGame = () => {
-    totalQuestions = [...questions]
-    score = 0
-    newQuestion()
+                formatQuestion.answer = Math.floor(Math.random() * 3) + 1
+
+                const answerChoices = [...loadedQuestion.incorrect_answers]
+                answerChoices.splice(
+                    formatQuestion.answer - 1, 0, loadedQuestion.correct_answer
+                ) //adds correct answer into the array
+                answerChoices.forEach((answer, index) => {
+                    formatQuestion["choice" + (index + 1)] = answer
+                })
+
+                return formatQuestion
+            })
+            startGame()
+        })
+
+    startGame = () => {
+        totalQuestions = [...questions]
+        score = 0
+        newQuestion()
+    }
 }
 
 
@@ -97,7 +106,7 @@ answers.forEach(answer => {
             outcome = 'correct'
             score++
         }
-        
+
         selection.parentElement.classList.add(outcome)
         let outcomeText = document.getElementById("outcome")
         outcomeText.innerText = outcome
